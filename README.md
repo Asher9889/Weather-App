081f6cde03c651d305963caf0d76e9c3d62acf31
Asher9889 <saurabhkushwaha9889@gmail.com>
2024-02-25 14:23:46 +0530

    first Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+I made a WeatherApp using HTML, Css, JavaScript. 
---
+++ script.js
@@ -0,0 +1,47 @@
+const apiKey = "cc3d8ed8469ad2d068b93a8a90db0397";
+const apiUrl =  "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+async function getData (city) {
+  try{
+  const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
+  const data = await response.json();
+  console.log(data)
+  
+
+  document.querySelector("#temperature").innerText = data.main.temp + "°c";
+  document.querySelector("#city").innerText = data.name;
+  document.querySelector("#wind-speed").innerText = data.wind.speed + " km/h";
+  document.querySelector("#humidity").innerText = data.main.humidity + "%";
+
+  const imgIcon = document.querySelector("#weatherIcon");
+  if(data.weather[0].main == "Haze") {
+  imgIcon.src = "/images/mist.png";
+  } else if (data.weather[0].main == "clear") {
+    imgIcon.src = "/images/clear1.png";
+  } else if (data.weather[0].main == "clouds") {
+    imgIcon.src = "/images/clouds.png";
+  } else if (data.weather[0].main == "drizzle") {
+    imgIcon.src = "/images/drizzle.png";
+  } else if (data.weather[0].main == "rain") {
+    imgIcon.src = "/images/rain.png";
+  } else if (data.weather[0].main == "snow") {
+    imgIcon.src = "/images/snow.png";
+  } else if (data.weather[0].main == "thunderstorm") {
+    imgIcon.src = "/images/thunderstorm.png";
+  }
+
+  } catch(e) {
+    console.log(prompt("City Not Found " + e));
+    // console.log(e);
+  }
+
+  
+ 
+}
+
+const searchBox = document.querySelector("input");
+const searchBtn = document.querySelector(".search-btn");
+searchBtn.addEventListener("click", () => {
+  getData(searchBox.value);
+})
+
